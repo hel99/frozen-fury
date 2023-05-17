d6c3373a876e86d40368f90d7f215e4894a3d62d
andjv <avesic76@gmail.com>
2023-05-17 12:51:06 +0200

    Umiranje foke udarcem pingvina part2

--- Assets/Scripts/EnemySeal.cs
+++ Assets/Scripts/EnemySeal.cs
@@ -1,13 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySeal : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(ShouldDieFromCollision(collision))
+        {
+            Die();
+        }      
+    }
+
+    private bool ShouldDieFromCollision(Collision2D collision)
     {
         Penguin penguin = collision.gameObject.GetComponent<Penguin>();
         if (penguin != null)
-            gameObject.SetActive(false);
+            return true;
+
+        return false;
+    }
+
+    void Die()
+    {
+        gameObject.SetActive(false);
     }
 }
