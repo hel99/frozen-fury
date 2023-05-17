8be55bc822350c61363ad41d32c3b40bd5f1a670
hel99 <helena.aganovic99@gmail.com>
2023-05-17 10:33:43 +0200

    Promena boje pingvina klikom na mis

--- Assets/Scripts/Penguin.cs
+++ Assets/Scripts/Penguin.cs
@@ -10,6 +10,19 @@ public class Penguin : MonoBehaviour
         GetComponent<Rigidbody2D>().isKinematic = true;
     }
 
+    void OnMouseDown()
+    {
+
+        GetComponent<SpriteRenderer>().color = new Color(0,150,255);
+
+    }
+
+    void OnMouseUp()
+    {
+
+        GetComponent<SpriteRenderer>().color = new Color(255, 255, 255);
+    }
+
     // Update is called once per frame
     void Update()
     {
